4b9a07110fafacbc1cbbddd9c83f303af4d7dbe8
Rasyah175 <rasyahodhe450@gmail.com>
2020-06-03 15:07:28 +0700

    13020170045/La Ode Rahimsyah/Mid-test

---
+++ app/build/generated/source/buildConfig/debug/com/laode/tugasproject4/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.laode.tugasproject4;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.laode.tugasproject4";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
